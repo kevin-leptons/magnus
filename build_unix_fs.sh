a7721a78956d21860167d42e117901fecc3b6eea
Kevin Leptons <kevin.leptons@gmail.com>
2018-07-07 20:29:30 +0700

    build unix file system for both debian and macos

--- build_unix_fs.sh
+++ build_unix_fs.sh
@@ -8,7 +8,21 @@ git submodule update
 source package/spec.sh
 
 BUILD_DATE=$(date)
+UNAME=$(uname)
 
+if [[ "$UNAME" == "Linux" ]]; then
+    function sed_replace() {
+        sed -i -e "$1" "$2"
+    }
+elif [[ "$UNAME" == "Darwin" ]]; then
+    function sed_replace() {
+        sed -i "" -e "$1" "$2"
+    }
+else
+    echo "$UNAME" is not supported
+    exit 1
+fi
+echo $SED_NO_BACKUP
 rm -rf dest dist
 mkdir -vp dest dist
 
@@ -16,13 +30,13 @@ mkdir -vp dest/etc
 cp -r vim dest/etc/magnus
 
 VIM_PLUGIN_DIR="\/etc\/$PKG_NAME"
-sed -i '' -e "s/\$VIM_PLUGIN_DIR/${VIM_PLUGIN_DIR}/g" dest/etc/magnus/vimrc
+sed_replace "s/\$VIM_PLUGIN_DIR/${VIM_PLUGIN_DIR}/g" dest/etc/magnus/vimrc
 
 MAN_DIR=dest/usr/share/man/man1
 MAN_FILE=$MAN_DIR/magnus.1
 mkdir -vp $MAN_DIR
 cp doc/magnus.1.man $MAN_FILE
-sed -i '' -e "s/\$PKG_NAME/${PKG_NAME}/g" $MAN_FILE
-sed -i '' -e "s/\$PKG_VERSION/${PKG_VERSION}/g" $MAN_FILE
-sed -i '' -e "s/\$BUILD_DATE/${BUILD_DATE}/g" $MAN_FILE
+sed_replace "s/\$PKG_NAME/${PKG_NAME}/g" $MAN_FILE
+sed_replace "s/\$PKG_VERSION/${PKG_VERSION}/g" $MAN_FILE
+sed_replace "s/\$BUILD_DATE/${BUILD_DATE}/g" $MAN_FILE
 gzip $MAN_FILE
