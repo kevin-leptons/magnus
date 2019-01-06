715bdc8ca46e2343d262cc3fd48d04c22171cc5e
Kevin Leptons <kevin.leptons@gmail.com>
2019-01-06 18:25:30 +0700

    check build files before pack

--- dist.sh
+++ dist.sh
@@ -4,6 +4,11 @@ set -e
 
 source package/spec.sh
 
+if ! [ -d dest ]; then
+    echo "Use 'make' to build first"
+    exit 1
+fi
+
 cp -r package/debian dest/DEBIAN
 sed -i -e "s/\$PKG_NAME/${PKG_NAME}/g" dest/DEBIAN/control
 sed -i -e "s/\$PKG_VERSION/${PKG_VERSION}/g" dest/DEBIAN/control
