6c4fa716a0655d60b58f101f75fc337436d84877
Kevin Leptons <kevin.leptons@gmail.com>
2020-01-12 02:25:40 -0800

    change control directory before build debian

--- dist_debian.sh
+++ dist_debian.sh
@@ -9,4 +9,5 @@ sed_replace "s/\$PKG_NAME/${PKG_NAME}/g" dest/DEBIAN/control
 sed_replace "s/\$PKG_VERSION/${PKG_VERSION}/g" dest/DEBIAN/control
 
 mkdir -vp dist
+chmod -R 0755 dest dist
 dpkg-deb --build -D dest/ dist/${PKG_NAME}_${PKG_VERSION}-0_all.deb
